b76c3ea6bf37a96d267cc319397ca9f626067da8
Naveen <naveen.k.manickam@gmail.com>
2018-06-26 08:30:00 +0530

    Review Comments from Rob 06/25

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -5,15 +5,15 @@ contract Splitter {
     address public receiver1;
     /* Receiver2 address */
     address public receiver2;
-    event regRec(address indexed);
-    event tranEvent(address indexed receiver, uint256 value); 
+    event LogRegRec(address indexed);
+    event LogTranEvent(address indexed receiver, uint256 value); 
     /* constructor */
     constructor (address _rec1, address _rec2) public {
         /* Setup the reciever accounts */
         receiver1 = _rec1;
-        emit regRec(receiver1);
+        emit LogRegRec(receiver1);
         receiver2 = _rec2;
-        emit regRec(receiver2);
+        emit LogRegRec(receiver2);
     }
     
     function splitFunds() public payable {
@@ -22,11 +22,11 @@ contract Splitter {
         /* Transferring funds to the first receiver */ 
         receiver1.transfer(msg.value/2);
         /* Logging split transfer for each of the recievers */
-        emit tranEvent(receiver1,msg.value/2);
+        emit LogTranEvent(receiver1,msg.value/2);
         /* Transferring funds to the second receiver */  
         receiver2.transfer(msg.value/2);
         /* Logging split transfer for each of the recievers */
-        emit tranEvent(receiver2,msg.value/2);
+        emit LogTranEvent(receiver2,msg.value/2);
         }
     }
 
